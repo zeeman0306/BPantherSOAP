669395612b53d723e11bcb93b1467024e240d09b
zeeman0306 <zeeman0306@gmail.com>
2018-07-17 11:55:22 -0400

    added files

---
+++ src/main/java/com/zamir/BPantherSOAP/BPantherSoapApplication.java
@@ -0,0 +1,12 @@
+package com.zamir.BPantherSOAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BPantherSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BPantherSoapApplication.class, args);
+	}
+}
